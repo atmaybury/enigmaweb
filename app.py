7032f42f549a4b30520d9acb9c5f5ce745e4e26f
atmaybury <atmaybury@protonmail.com>
2017-11-15 14:44:23 +1100

    removed duplicates, still fixing css linking issue

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    """
-    return render_template("index.html")
-    """
+
     if request.method == "POST":
         plaintext = request.form.get("plaintext")
         ciphertext = enigma(plaintext)
         return render_template("index.html", ciphertext=ciphertext)
+
     if request.method == "GET":
         return render_template("index.html", ciphertext="")
-

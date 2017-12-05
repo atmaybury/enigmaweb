b031e7f68e37584ff9ea681d984fcc39de8e070b
atmaybury <atmaybury@protonmail.com>
2017-12-05 20:34:06 +1100

    rejects empty input fields

--- static/scripts.js
+++ static/scripts.js
@@ -2,10 +2,18 @@ $(document).ready(function(){
 
     // show ciphertext box when form is submitted
     // TODO: Ajax so no page reload
-    $("#ciphertext").hide();
+    //$("#ciphertext").hide();
 
-    $("#submit").click(function(){
-        $("#ciphertext").slideDown();
+    //$("#submit").click(function(){
+    //    $("#ciphertext").slideDown();
+    //});
+
+    // alert and prevent submit if any empty fields
+    $("#inputForm").submit(function() {
+        if ($.trim($("#plaintext").val()) == "" || $.trim($("#r0").val()) == "" || $.trim($("#r1").val()) == "" || $.trim($("#r2").val()) == "" ) {
+            alert("Please fill all fields!");
+            return false;
+        }
     });
- 
+
 });

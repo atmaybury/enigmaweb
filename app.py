c462db3ca64c68a9224e586cbfa9fe29674655ab
atmaybury <atmaybury@protonmail.com>
2017-12-08 23:34:04 +1100

    implemented jinja templates, added an about page and a footer

--- app.py
+++ app.py
@@ -27,6 +27,12 @@ def index():
     if request.method == "GET":
         return render_template("index.html", ciphertext="")
 
+
+@app.route("/about")
+def about():
+    
+    return render_template("about.html")
+
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=80)
     app.run()
